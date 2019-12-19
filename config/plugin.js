9ae125b3a31bef3dca8dd3644f432f3e47abc1af
rojer <rojerchen@qq.com>
2019-12-19 19:23:51 +0800

    adjust permission

--- config/plugin.js
+++ config/plugin.js
@@ -1,46 +1,46 @@
-'use strict';
-
+"use strict";
+const path = require("path");
 exports.routerPlus = {
   enable: true,
-  package: 'egg-router-plus',
+  package: "egg-router-plus"
 };
 
 exports.cors = {
   enable: true,
-  package: 'egg-cors',
+  package: "egg-cors"
 };
 
 /**
  * 以下为 coca 框架插件包
  */
 
-
 //  数据库
 exports.sequelize = {
   enable: true,
-  package: 'egg-coca-sequelize',
+  package: "egg-coca-sequelize"
 };
 
 // restful
 exports.cocaRestful = {
   enable: true,
-  package: 'egg-coca-restful',
+  package: "egg-coca-restful"
 };
 
 // 上传
 exports.cocaUpload = {
   enable: true,
-  package: 'egg-coca-upload',
+  package: "egg-coca-upload"
 };
 
 // 后台 用户-角色-权限 快速开发包
 exports.cocaUserPermission = {
   enable: true,
-  package: 'egg-coca-user-permission',
+  package: "egg-coca-user-permission"
+  // path: path.join(__dirname, "../lib/egg-coca-user-permission")
 };
 
 // 其他工具包
 exports.cocaHelper = {
   enable: true,
-  package: 'egg-coca-helper',
+  package: "egg-coca-helper"
 };
